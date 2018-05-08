3112b20e61eb81470b692543085341b8aef0751c
Price Comstock <price.comstock@gmail.com>
2018-05-07 22:13:22 -0500

    allowed MORE CORS

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ class MainHandler(tornado.web.RequestHandler):
 class EloHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
 
     def get(self):
         self.write(league.elo_history_json())
